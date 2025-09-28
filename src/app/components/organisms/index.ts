fcee2c6d587cde6dd2bc67262f350e474f14142b
Naoya Noguchi <naoya.work0213@gmail.com>
2025-09-29 07:12:53 +0900

    fv変更_20250929_07-12

--- src/app/components/organisms/index.ts
+++ src/app/components/organisms/index.ts
@@ -3,7 +3,7 @@ export { default as Header } from "./header/Header";
 
 // main
 export { default as SectionAbout } from "./main/SectionAbout";
-export { default as SectionFv } from "./main/SectionFv";
+export { default as SectionFv } from "./main/fv/SectionFv";
 export { default as SectionLike } from "./main/SectionLike";
 export { default as SectionSkill } from "./main/SectionSkill";
 export { default as SectionWork } from "./main/SectionWork";
