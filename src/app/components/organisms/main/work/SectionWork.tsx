5bbd7e8fc6bd3324881e2b3479b4789d42927d03
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-04 01:19:42 +0900

    更新_20251004_01-19

--- src/app/components/organisms/main/work/SectionWork.tsx
+++ src/app/components/organisms/main/work/SectionWork.tsx
@@ -3,7 +3,7 @@ import SectionCard from "@/app/components/molecules/SectionCard";
 const SectionWork = () => {
   return (
     <div>
-      <SectionCard title="Works -" subTitle="これまで作成したサイト実績">
+      <SectionCard title="Works -" subTitle="これまで作成した実績">
         <div></div>
       </SectionCard>
     </div>
