f44b8095eba20a445c1d99479289a602046f9c97
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-04 14:26:38 +0900

    仮_20251004_14-26

--- src/app/components/atoms/ButtonBrown.tsx
+++ src/app/components/atoms/ButtonBrown.tsx
@@ -1,10 +1,14 @@
 type ButtonBrownProps = {
   text: string;
+  onClick?: () => void;
 };
 
-const ButtonBrown = ({ text }: ButtonBrownProps) => {
+const ButtonBrown = ({ text, onClick }: ButtonBrownProps) => {
   return (
-    <div className="pc:h-10 flex h-[35px] w-fit cursor-pointer items-center rounded-full border border-[#795549] bg-[#795549] px-5 py-[3px] text-sm font-bold text-[#f3f0eb] transition-colors duration-500 hover:bg-[#d7cdbe] hover:text-[#795549]">
+    <div
+      className="pc:h-10 flex h-[35px] w-fit cursor-pointer items-center rounded-full border border-[#795549] bg-[#795549] px-5 py-[3px] text-sm font-bold text-[#f3f0eb] transition-colors duration-500 hover:bg-[#d7cdbe] hover:text-[#795549]"
+      onClick={onClick}
+    >
       {text}
     </div>
   );
--- src/app/components/organisms/main/about/SectionAboutModal.tsx
+++ src/app/components/organisms/main/about/SectionAboutModal.tsx
@@ -1,11 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
 import ButtonBrown from "@/app/components/atoms/ButtonBrown";
+import Modal from "@/app/components/atoms/modal/modal";
 
 const SectionAboutModal = () => {
+  const [openWhich, setOpenWhich] = useState<null | "past" | "next">(null);
+
   return (
-    <div className="flex flex-col items-center justify-center gap-5 sm:flex-row sm:gap-8">
-      <ButtonBrown text="これまでの学習内容"></ButtonBrown>
-      <ButtonBrown text="これから学習したい事"></ButtonBrown>
-    </div>
+    <>
+      <div className="flex flex-col items-center justify-center gap-5 sm:flex-row sm:gap-8">
+        <ButtonBrown
+          text="これまでの学習内容"
+          onClick={() => setOpenWhich("past")}
+        ></ButtonBrown>
+        <ButtonBrown
+          text="これから学習したい事"
+          onClick={() => setOpenWhich("next")}
+        ></ButtonBrown>
+      </div>
+
+      {/* モーダル */}
+      <Modal
+        open={openWhich === "past"}
+        onClose={() => setOpenWhich(null)}
+        title="これまでの学習内容"
+      >
+        <div></div>
+      </Modal>
+    </>
   );
 };
 
