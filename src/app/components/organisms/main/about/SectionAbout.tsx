ec28588c03fe2f853336ed0bb1f024a70ba4786a
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-04 10:09:57 +0900

    ボタン_20251004_10-09

--- src/app/components/organisms/main/about/SectionAbout.tsx
+++ src/app/components/organisms/main/about/SectionAbout.tsx
@@ -47,8 +47,9 @@ const SectionAbout = () => {
             </div>
           </div>
           {/* 詳細ボタン */}
-          <div>
-            <ButtonBrown text="ボタン"></ButtonBrown>
+          <div className="flex flex-col items-center justify-center gap-5 sm:flex-row sm:gap-8">
+            <ButtonBrown text="これまでの学習内容"></ButtonBrown>
+            <ButtonBrown text="これから学習したい事"></ButtonBrown>
           </div>
         </div>
       </SectionCard>
