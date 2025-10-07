fc6b053e75f2e84bb103b8ba5aa606cbdb6a36d2
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-07 14:57:55 +0900

    like_20251007_14-57

--- src/app/components/organisms/main/like/SectionLike.tsx
+++ src/app/components/organisms/main/like/SectionLike.tsx
@@ -4,7 +4,49 @@ const SectionLike = () => {
   return (
     <div>
       <SectionCard title="Like -" subTitle="好きな事・趣味">
-        <div></div>
+        <div className="mt-5 px-2 max-[374px]:mt-1">
+          <div className="flex flex-col">
+            {/* スノーボード */}
+            <div className="flex flex-col gap-3 border-b border-[#d7cdbe] px-3 py-5 sm:flex-row">
+              {/* タイトル */}
+              <div className="flex gap-2 sm:min-w-40">
+                <div className="flex h-5 w-5 items-center justify-center rounded-full bg-[#795549]">
+                  <p className="text-xs text-[#f3f0eb]">1</p>
+                </div>
+                <h2 className="font-bold">スノーボード</h2>
+              </div>
+
+              {/* 内容 */}
+              <div>
+                <p>
+                  高校3年からスノーボードにハマっています。冬になれば、ほぼ毎週行くくらい好きです！これまで、29か所ものスキー場を訪問しました。今シーズンの目標は、北海道のニセコに滑りに行くこと！
+                </p>
+              </div>
+            </div>
+
+            {/* カメラ */}
+            <div className="flex flex-col gap-3 px-3 py-5 sm:flex-row">
+              {/* タイトル */}
+              <div className="flex gap-2 sm:min-w-40">
+                <div className="flex h-5 w-5 items-center justify-center rounded-full bg-[#795549]">
+                  <p className="text-xs text-[#f3f0eb]">2</p>
+                </div>
+                <h2 className="font-bold">カメラ</h2>
+              </div>
+
+              {/* 内容 */}
+              <div>
+                <p>
+                  <span className="font-bold">
+                    念願のGR3！カメラとともに旅へ。
+                  </span>
+                  <br></br>
+                  特別な場所や日常のひとコマ、切り取るたびに広がる新しい世界。その中から、お気に入りの写真を少しだけご紹介します！
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </SectionCard>
     </div>
   );
