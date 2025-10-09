603258383f0ec854f34b8274b891aa2f903b1bfe
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-09 23:09:53 +0900

    削除_20251009_23-09

--- src/app/components/organisms/main/like/SectionLike.tsx
+++ src/app/components/organisms/main/like/SectionLike.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import SectionCard from "@/app/components/molecules/section_card/SectionCard";
 
 const SectionLike = () => {
@@ -9,7 +7,7 @@ const SectionLike = () => {
         <div className="mt-5 px-2 max-[374px]:mt-1">
           <div className="m-auto flex max-w-screen-sm flex-col">
             {/* スノーボード */}
-            <div className="relative flex flex-col gap-3 border-b border-[#d7cdbe] px-3 py-5 sm:flex-row">
+            <div className="flex flex-col gap-3 border-b border-[#d7cdbe] px-3 py-5 sm:flex-row">
               {/* タイトル */}
               <div className="flex gap-2 sm:min-w-40">
                 <div className="flex h-5 w-5 items-center justify-center rounded-full bg-[#795549]">
@@ -21,27 +19,8 @@ const SectionLike = () => {
               {/* 内容 */}
               <div>
                 <p>
-                  高校3年からスノーボードにハマっています。冬になれば、ほぼ毎週行くくらい好きです！これまで、29か所ものスキー場を訪問しました。今シーズンの目標は、北海道のニセコに滑りに行くこと！
+                  高校3年からスノーボードにハマっています。冬になれば、ほぼ毎週ゲレンデに行きます。これまで、29か所ものスキー場を訪問しました。今シーズンの目標は、北海道のニセコに滑りに行くこと！
                 </p>
-              </div>
-
-              {/* デコレーション */}
-              {/* TODO　調整 */}
-              <div>
-                <Image
-                  src="/img/section_like/ski_icon.png"
-                  alt="スキーアイコン"
-                  width={30}
-                  height={30}
-                  className="absolute -top-10 right-24"
-                />
-                <Image
-                  src="/img/section_like/snowboard_icon.png"
-                  alt="スキーアイコン"
-                  width={30}
-                  height={30}
-                  className="absolute -top-7 right-12"
-                />
               </div>
             </div>
 
