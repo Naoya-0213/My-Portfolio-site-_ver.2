01ffb0d913f0de081bb46f8e00d3ad70b320fb27
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-11 21:10:48 +0900

    swiper再作成_20251011_21-10

--- src/app/components/atoms/swiper/WorkSwiper.tsx
+++ src/app/components/atoms/swiper/WorkSwiper.tsx
@@ -1,90 +1,31 @@
-// src/app/components/atoms/work_swiper/WorkSwiper.tsx
-import "swiper/css";
+
+
+"use client";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import WorkSwiperSlide from "../work_swiper_slide/WorkSwiperSlide";
-import styles from "./workSwiper.module.scss";
+import "../../../../../node_modules/swiper/swiper.css";
 
-/**
- * 中央の1枚をフル表示し、左右は“チラ見せ”するスライダー。
- * ポイント:
- * - slidesPerView="auto" + CSSで .swiper-slide の固定幅を指定
- * - centeredSlides で中央寄せ
- * - 親に overflow:hidden を付けて左右を切り落とす
- */
-export default function WorkSwiper() {
+const data: string[] = ["Slide 1", "Slide 2", "Slide 3", "Slide 4"];
+
+function App() {
   return (
-    <div className={styles.peekCarousel}>
+    <>
       <Swiper
-        //  modules={[Autoplay, Pagination, Navigation]}  // ① React特有：必要モジュールを明示
-        slidesPerView="auto"                           // ② 常に auto（幅はCSSで固定）
-        centeredSlides                                  // ③ 中央寄せ（Boundsは一旦オフ）
-        // centeredSlidesBounds                         // ← jQuery版は使っていないので一旦外す
-        spaceBetween={20}
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
         loop
-        grabCursor
-        // jQuery版の autoplay 相当
-        autoplay={{
-          delay: 3000,
-          stopOnLastSlide: false,
-          disableOnInteraction: true,
-          reverseDirection: false,
-        }}
-        // ③ 余白をSwiperに伝える（親paddingの代わり）
-        slidesOffsetBefore={16}
-        slidesOffsetAfter={16}
-
-        // ④ 端で止まらない保険（可変幅ループの安定化）
-        loopPreventsSliding={false}
-        loopAdditionalSlides={4}
-
-        // （必要なら）境界幅での余白調整だけ breakpoint で
-        breakpoints={{
-          768: { spaceBetween: 24, slidesOffsetBefore: 24, slidesOffsetAfter: 24 },
-        }}
-
-        // オプション：矢印やドットを使う場合（クラス名はDOM側のものに合わせる）
-        pagination={{ el: ".work__swiper-pagination", clickable: true }}
-        navigation={{
-          prevEl: ".work__swiper-button-prev",
-          nextEl: ".work__swiper-button-next",
-        }}
-        observer
-        observeParents
-        initialSlide={0}
       >
-        {/* sabolon */}
-        <SwiperSlide className={styles.slide}>
-          <WorkSwiperSlide
-            pc_img="/img/section_work/sabolon/sabolon-pc.png"
-            sp_img="/img/section_work/sabolon/sabolon.png"
-          />
-        </SwiperSlide>
-
-        {/* oha */}
-        <SwiperSlide className={styles.slide}>
-          <WorkSwiperSlide
-            pc_img="/img/section_work/oha/oha-pc.png"
-            sp_img="/img/section_work/oha/oha.png"
-          />
-        </SwiperSlide>
-
-        {/* onomichi */}
-        <SwiperSlide className={styles.slide}>
-          <WorkSwiperSlide
-            pc_img="/img/section_work/onomichi/onomichi-pc.png"
-            sp_img="/img/section_work/onomichi/onomichi.png"
-          />
-        </SwiperSlide>
-
-        {/* ポートフォリオサイト */}
-        <SwiperSlide className={styles.slide}>
-          <WorkSwiperSlide
-            pc_img="/img/section_work/naoya-portfolio/naoya-portfolio-pc.png"
-            sp_img="/img/section_work/naoya-portfolio/naoya-portfolio.png"
-          />
-        </SwiperSlide>
+        {data.map((d, index) => (
+          <SwiperSlide key={index}>
+            <div style={{ background: "grey", height: "300px" }}>{d}</div>
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </div>
+    </>
   );
 }
+
+export default App;
