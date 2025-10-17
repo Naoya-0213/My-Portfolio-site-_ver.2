83e2f6727b642378a45918e00e61fe2c26ec96e0
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-17 11:27:01 +0900

    ヘッダー更新_20251017_11-27

--- src/app/(naoya-portfolio)/work/oha/page.tsx
+++ src/app/(naoya-portfolio)/work/oha/page.tsx
@@ -1,5 +1,6 @@
 import { ohaContents } from "@/const/work/ohaContents";
 
+import { Header } from "@/app/components/organisms";
 import OtherWorks from "@/app/components/organisms/other_works/OtherWorks";
 
 import WorkSectionCard from "../components/WorkSectionCard";
@@ -7,6 +8,7 @@ import WorkSectionCard from "../components/WorkSectionCard";
 const ohaPage = () => {
   return (
     <div className="flex flex-col gap-5">
+      <Header />
       <WorkSectionCard data={ohaContents} />
 
       <OtherWorks />
--- src/app/(naoya-portfolio)/work/portfolio/page.tsx
+++ src/app/(naoya-portfolio)/work/portfolio/page.tsx
@@ -3,10 +3,12 @@ import { portfolioContents } from "@/const/work/portfolioContents";
 import OtherWorks from "@/app/components/organisms/other_works/OtherWorks";
 
 import WorkSectionCard from "../components/WorkSectionCard";
+import { Header } from "@/app/components/organisms";
 
 const PortfolioPage = () => {
   return (
     <div className="flex flex-col gap-5">
+      <Header />
       <WorkSectionCard data={portfolioContents} />
 
       <OtherWorks />
