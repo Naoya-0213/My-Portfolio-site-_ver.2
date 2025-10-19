6d11b196122f35d02a3ea2fd729e236db531d466
Naoya Noguchi <naoya.work0213@gmail.com>
2025-10-20 00:11:31 +0900

    写真追加_20251020_00-11

--- src/const/like/gr3Contents.ts
+++ src/const/like/gr3Contents.ts
@@ -1,28 +1,62 @@
-export const NAV = [
+type types = {
+  date: string;
+  text: string;
+  pic: string;
+};
+
+export const gr3Contents: types[] = [
+  // 写真1
   {
-    href: "#fv",
-    label: "Home -",
-    icon: "/img/icon/home/paper-plane_icon-brown.png",
+    pic: "/img/section_like/pic/camera__picture1.png",
+    date: "2024年3月",
+    text: "念願のGR3を購入！",
   },
+  // 写真2
   {
-    href: "#skill",
-    label: "Skill -",
-    icon: "/img/icon/skill/skill_icon-brown.png",
+    pic: "/img/section_like/pic/camera__picture2.png",
+    date: "2024年5月",
+    text: "ハリーポッターでお馴染みのバタービール！",
   },
-  { href: "#work", label: "Work -", icon: "/img/icon/work/pc_icon-brown.png" },
+  // 写真3
   {
-    href: "#about",
-    label: "About me -",
-    icon: "/img/icon/about-me/user_icon-brown.png",
+    pic: "/img/section_like/pic/camera__picture3.png",
+    date: "2024年8月",
+    text: "夏の一枚。カラフルで綺麗!",
   },
+  // 写真4
   {
-    href: "#like",
-    label: "Like -",
-    icon: "/img/icon/camera/camera.png",
+    pic: "/img/section_like/pic/camera__picture4.png",
+    date: "2024年9月",
+    text: "愛媛県下灘駅の踏切にて。よい天気〜",
   },
+  // 写真5
   {
-    href: "#contact",
-    label: "Contact -",
-    icon: "/img/icon/mail/email_icon-brown.png",
+    pic: "/img/section_like/pic/camera__picture5.png",
+    date: "2024年9月",
+    text: "可愛くて即決購入！笑",
   },
+  // 写真6
+  {
+    pic: "/img/section_like/pic/camera__picture6.png",
+    date: "2024年9月",
+    text: "ハワイ旅行中にて。海が綺麗すぎた..",
+  },
+  // 写真7
+  {
+    pic: "/img/section_like/pic/camera__picture7.png",
+    date: "2024年11月",
+    text: "ハリーポッターで出てくるあの駅...！",
+  },
+  // 写真8
+  {
+    pic: "/img/section_like/pic/camera__picture8.png",
+    date: "2024年11月",
+    text: "実はfv画像も自分で撮影してます！",
+  },
+  // 写真9
+  //   {
+  //     pic: "/img/section_like/pic/camera__picture9.png",
+  //     date: "",
+  //     text: "",
+  //   },
 ];
